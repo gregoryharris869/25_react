f13cc126d5bfd63d533c2c18b214172db701d2d1
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-02-06 16:36:39 -0600

    Update RandomColor.jsx

--- src/components/random_color/RandomColor.jsx
+++ src/components/random_color/RandomColor.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+
 const RandomColor = () => {
-  return <div>RandomColor</div>;
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const { color, setColor } = useState("");
+
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleCreateRandomHexColor() {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)];
+    }
+    setColor(hexColor);
+  }
+
+  function handleCreateRandomRgbColor() {}
+  return (
+    <div
+      style={{ width: "100vw", height: "100vh", backgroundColor: "#000000" }}
+    >
+      <button onClick={() => setTypeOfColor("hex")}>Hex Color</button>
+      <button onClick={() => setTypeOfColor("rgb")}>RGB Color</button>
+      <button
+        onClick={() =>
+          typeOfColor === "hex"
+            ? handleCreateRandomHexColor
+            : handleCreateRandomRgbColor
+        }
+      >
+        Random Color
+      </button>
+    </div>
+  );
 };
 
 export default RandomColor;
