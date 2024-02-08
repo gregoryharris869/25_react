850fbfd51c22b4a1bb542027d915eb98babbfa1b
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-02-08 15:47:53 -0600

    Image Slider Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 // import Accordion from "./components/accordion/Accordion";
 // import RandomColor from "./components/random_color/RandomColor";
-import StarRating from "./components/star_rating/StarRating";
+// import StarRating from "./components/star_rating/StarRating";
+
+import ImageSlider from "./components/image_slider/ImageSlider";
 
 function App() {
   return (
     <>
       {/* <Accordion /> */}
       {/* <RandomColor /> */}
-      <StarRating noOfStars={5} />
+      {/* <StarRating noOfStars={5} /> */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
