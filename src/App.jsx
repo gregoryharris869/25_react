ce84742127e75d416bf312b6b4228639815f4ae2
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-02-13 00:40:38 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@
 // import RandomColor from "./components/random_color/RandomColor";
 // import StarRating from "./components/star_rating/StarRating";
 // import ImageSlider from "./components/image_slider/ImageSlider";
-
-import LoadMoreData from "./components/load_more_data/LoadMoreData";
+// import LoadMoreData from "./components/load_more_data/LoadMoreData";
 
 function App() {
   return (
     <>
       {/* <Accordion /> */}
+
       {/* <RandomColor /> */}
+
       {/* <StarRating noOfStars={5} /> */}
+
       {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
       /> */}
-      <LoadMoreData />
+
+      {/* <LoadMoreData /> */}
     </>
   );
 }
