d4747415f3fa89283cf98ba731c52e25e0dccbae
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-02-24 14:59:09 -0600

    3:17

--- src/components/github_profile_finder/GithubProfile.jsx
+++ src/components/github_profile_finder/GithubProfile.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import "./styles.css";
+
 const GithubProfile = () => {
-  return <div>GithubProfile</div>;
+  const [userName, setUserName] = useState("sangammukherjee");
+  return (
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="search-by-username"
+          placeholder="Search Github Username"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+    </div>
+  );
 };
 
 export default GithubProfile;
